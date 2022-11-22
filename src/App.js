5aa5c600b5cb07c85f6ee9a87f40f0098b1e53cb
Candice Serate <cwserate@gmail.com>
2022-11-21 22:11:41 -0500

    added a Navbar and Product Detail page while working more with routes, used Link to create a stationary navbar across the pages

--- src/App.js
+++ src/App.js
@@ -2,42 +2,23 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Cart from './Components/Cart/Cart';
 import Homepage from './Components/Homepage/Homepage';
+import Navbar from './Components/Navbar/Navbar';
+import ProductDetail from './Components/Product Detail/ProductDetail';
 import Product from './Components/Product/Product';
 
 
 function App() {
 
-  /*const product1 = {
-    "id": 1,
-    "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-    "price": 109.95,
-    "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-    "category": "men's clothing",
-    "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-    "rating": {
-    "rate": 3.9,
-    "count": 120
-    }
-  }
-  const product2 = {
-    "id": 2,
-    "title": "JanSport",
-    "price": 49.95,
-    "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-    "category": "accessories",
-    "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-    "rating": {
-    "rate": 3.9,
-    "count": 120
-    }*/
-  
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Routes>
-          <Route path= '/products' element= {<Homepage />} />
+          <Route exact path= '/' element= {<Homepage />} />
           <Route path= '/cart' element= {<Cart />} />
+          <Route path= '/detail' element= {<ProductDetail />} />
         </Routes>
+        
       </BrowserRouter>
      
       
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+        <Link to= '/'>Homepage</Link>
+        <Link to= '/cart'>Cart</Link>
+        <Link to= '/detail'>Product Detail</Link>
+
+
+    </div>
+  )
+}
+
+export default Navbar
